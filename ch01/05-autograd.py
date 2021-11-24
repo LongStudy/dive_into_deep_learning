d17213b4418ce19d49704949862b1e66c878d6e6
Study Long <xzyxzy0224@outlook.com>
2021-11-24 20:29:30 +0800

    更改

--- ch01/05-autograd.py
+++ ch01/05-autograd.py
@@ -7,27 +7,41 @@ print('x.grad:', x.grad)
 y = 2 * torch.dot(x, x)  # 2.记录目标值的计算
 print('y:', y)
 y.backward()  # 3.执行它的反向传播函数
+print('x:', x)
 print('x.grad:', x.grad)  # 4.访问得到的梯度
 print('x.grad == 4*x:', x.grad == 4 * x)
+print('')
 
 ## 计算另一个函数
-x.grad.zero_()
+x.grad.zero_() # 清零
 y = x.sum()
 print('y:', y)
 y.backward()
 print('x.grad:', x.grad)
+print('')
 
 # 非标量变量的反向传播
 x.grad.zero_()
 print('x:', x)
 y = x * x
-y.sum().backward()
+y.sum().backward() # 梯度只能为标量（即一个数）输出隐式地创建，此时y为矩阵，需要sum为标量
 print('x.grad:', x.grad)
+print('')
 
-
+# 分离计算
+x.grad.zero_()
+y = x * x
+u = y.detach()
+z = u * x # u当成常量，所以x.grad = u
+z.sum().backward()
+print('x.grad == u', x.grad == u)
+x.grad.zero_()
+y.sum().backward()
+print('x.grad == 2 * x', x.grad == 2 * x)
+print('')
 def f(a):
     b = a * 2
-    print(b.norm())
+    print('b.norm:', b.norm())
     while b.norm() < 1000:  # 求L2范数：元素平方和的平方根
         b = b * 2
     if b.sum() > 0:
